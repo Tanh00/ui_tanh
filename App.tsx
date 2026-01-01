5c57657698f98ec750d3dcfc804d569eb72d150c
Tran Tuan Anh <dtc225200078@2ictu.edu.vn>
2026-01-01 17:30:52 +0700

    tuananh

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import DangNhap from './src/screen/DangNhap';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <DangNhap />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
