b98db9a94a27607685fc76e89b9b3405e693e739
Tran Tuan Anh <dtc225200078@2ictu.edu.vn>
2026-01-07 09:50:51 +0700

    push

--- App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import DangNhap from './src/screen/UI1/DangNhap';
-import DangKy from './src/screen/UI1/DangKy';
-
-export default function App() {
-return <DangNhap />;
-}
